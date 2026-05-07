dea7928f498f13ff59e0144eba6c6d7001f6e9f6
LostSoul <miro.slezak0601@gmail.com>
2026-05-07 08:20:20 +0200

    Update run_planner.bat

--- run_planner.bat
+++ run_planner.bat
@@ -5,13 +5,15 @@ if not exist %VENV_DIR% (
     echo Creating virtual environment...
     python -m venv %VENV_DIR%
     call %VENV_DIR%\Scripts\activate
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt
-    pip install -r requirements-dev.txt
+
 ) else (
     echo Virtual environment found. Activating...
     call %VENV_DIR%\Scripts\activate
 )
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
 
 echo Setting up pre-commit hooks...
 pre-commit install
